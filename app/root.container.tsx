ea2b5cff59925e4e18a6fd9d947c0247eef4e93a
Artyom Trityak <art.trityak@gmail.com>
2016-05-02 17:28:35 -0700

    Eslint support

--- app/root.container.tsx
+++ app/root.container.tsx
@@ -2,11 +2,9 @@ import * as React from "react";
 
 
 export default class App extends React.Component<any, any> {
-    render() {
-        return (
-            <div>
-                Hello world
-            </div>  
-        ); 
-    }
+  render() {
+    return (
+      <div>Hello world</div>  
+    ); 
+  }
 }
